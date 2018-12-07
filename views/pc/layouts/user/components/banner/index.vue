f9042e0f785c7e13b0cf66a90b9119b0217259dc
DT\davidzhu <davidzhu@demo.com>
2018-12-07 19:13:41 +0800

    commit

---
+++ views/pc/layouts/user/components/banner/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="user-banner">
+    <div class="content">
+      <img src="./images/banner.png">
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "",
+  data() {
+    return {};
+  }
+};
+</script>
+<style lang="scss" scoped>
+.user-banner {
+  margin-bottom: 20px;
+  img {
+    display: block;
+    max-width: 100%;
+  }
+  // height: 389px;
+  // background: url(./images/banner.png) center center no-repeat;
+}
+</style>
